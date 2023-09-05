ed8589b30382f34c8ec20f955ac6d331c862fc7e
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-09-05 17:54:30 +0530

    100mhz

--- src/config.tcl
+++ src/config.tcl
@@ -47,7 +47,7 @@ set ::env(DECAP_CELL) "\
 # clock
 set ::env(RUN_CTS) 1
 # period is in ns, so 20ns == 50mHz
-set ::env(CLOCK_PERIOD) "20"
+set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) {clk}
 
 # hold/slack margin
