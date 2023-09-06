44f01e2a388a5ba36f698a911630b134a244b127
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-09-06 12:12:26 +0530

    module update

--- src/tt_um_vijayank88_arbiter_puf.v
+++ src/tt_um_vijayank88_arbiter_puf.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_nehamjhanwar_arbiter_puf  (
+module tt_um_vijayank88_arbiter_puf  (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7-segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
