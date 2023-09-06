2486fd886019a40aa7306eec07d161a24cdaa5a6
vijayan <85787913+vijayank88@users.noreply.github.com>
2023-09-06 16:09:32 +0530

    clength

--- src/tt_um_vijayank88_arbiter_puf.v
+++ src/tt_um_vijayank88_arbiter_puf.v
@@ -1,6 +1,9 @@
 `default_nettype none
 
-module tt_um_vijayank88_arbiter_puf  (
+module tt_um_vijayank88_arbiter_puf #(
+	parameter integer C_LENGTH = 8
+         ) 
+	(
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7-segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
